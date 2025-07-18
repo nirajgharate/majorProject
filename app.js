e682a4dd3828f76bb20c4c0c5f77ea84f4ee447c
nirajgharate <nirajgharate.dev@gmail.com>
2025-07-18 17:24:59 +0530

    this is my new commit

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const Listing = require('./models/listing.js');
 const path = require("path");
 const methodOverride = require("method-override");
 const engine = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 
@@ -29,10 +31,13 @@ app.get("/", (req,res) => {
     res.send("Server is working well");
 })
 
-app.get("/listings",async (req,res) => {
+app.get(
+    "/listings",
+    wrapAsync(async (req,res) => {
     const allListings = await Listing.find({});
         res.render("listings/index.ejs", {allListings});
-    });
+    })
+);
 
     // app.get("/listings/",async (req,res) => {
     // const allListings = await Listing.findMyId({});
@@ -45,44 +50,59 @@ app.get("/listings",async (req,res) => {
     });
 
     //show route
-    app.get("/listings/:id", async (req,res) => {
+    app.get(
+        "/listings/:id",
+        wrapAsync(async (req,res) => {
         let {id} = req.params;
         const listing = await Listing.findById(id);
         res.render("listings/show.ejs", {listing});
-    });
+    })
+);
 
     //Create Route
-    app.post("/listings", async(req,res,next) =>{
-        try{
-            const newListing = new Listing(req.body.listing);
+    app.post(
+        "/listings",
+        wrapAsync(async(req,res,next) =>{
+        if(!req.body.listing){
+            throw new ExpressError(400, "Send valid data for listing");
+        }
+        const newListing = new Listing(req.body.listing);
         await newListing.save();
         res.redirect("/listings");
-        }catch(err){
-            next(err);
-        }
-    });
+        
+    })
+);
 
     //edit route
-    app.get("/listings/:id/edit", async(req,res) => {
+    app.get(
+        "/listings/:id/edit",
+        wrapAsync(async(req,res) => {
         let {id} = req.params;
         const listing = await Listing.findById(id);
         res.render("listings/edit.ejs", { listing });
-    });
+    })
+);
 
     //update routes
-    app.put("/listings/:id", async(req,res) => {
+    app.put(
+        "/listings/:id",
+        wrapAsync(async(req,res) => {
         let {id} = req.params;
         await Listing.findByIdAndUpdate(id,{...req.body.listing});
         res.redirect(`/listings/${id}`);
-    });
+    })
+);
 
     //Delete Route
-    app.delete("/listings/:id", async(req,res) => {
+    app.delete(
+        "/listings/:id", 
+        wrapAsync(async(req,res) => {
         let {id} = req.params;
         let deleteListing = await Listing.findByIdAndDelete(id);
         console.log(deleteListing);
         res.redirect("/listings");
-    });
+    })
+);
 
 // app.get("/testListing",async(req,res) => {
 //     let sampleListing = new Listing({
@@ -97,9 +117,22 @@ app.get("/listings",async (req,res) => {
 //     res.send("successful testing");
 // });
 
-app.use((err,req,res,next) => {
-    res.send("Something went wrong");
+// app.all("*",(req,res,next) => {
+//     next(new ExpressError(404,"Page Not Found"));
+// }); 
+
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something went wrong" } = err;
+    console.log("Rendering error page with message:", message);
+    res.status(statusCode).render("error.ejs", {message});
 });
+
+// app.use((err,req,res,next) => {
+//     // res.send("Something went wrong");
+//     let{statusCode = 500, message = "Something went wrong"} = err;
+//     res.render("error.ejs",{message});
+//     // res.status(statusCode).send(message);
+// });
 
 app.listen(port,() => {
     console.log(`Server is running on port ${port}`);
--- models/listing.js
+++ models/listing.js
@@ -11,8 +11,20 @@ const listingSchema = new Schema({
         required: true
     },
     image: {
-        filename: String,
-        url: String 
+        filename: {type:String},
+        url : {
+            type:String,
+                    default:
+            "https://images.unsplash.com/photo-1439066615861-d1af74d74000?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGFrZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+            set: function (v){
+                if(!v || v.trim() === ""){
+                    return "https://images.unsplash.com/photo-1439066615861-d1af74d74000?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGFrZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+                }
+                return v;
+            }
+                
+
+        }
     },
     price: {
         type: Number,
