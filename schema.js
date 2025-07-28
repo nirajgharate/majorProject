d27e7733e4b24c9c4eab2c3515f3adf3a26c9290
nirajgharate <nirajgharate.dev@gmail.com>
2025-07-28 23:38:50 +0530

    this is project day c

--- schema.js
+++ schema.js
@@ -4,9 +4,9 @@ module.exports.listingSchema = Joi.object({
     listing : Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
-        location: Joi.string().required(),
+        location: Joi.string().url().required(),
         country: Joi.string().required(),
-        price: Joi.number().required().min(0),
+        price: Joi.number().required().min(0),node
         image: Joi.string().allow("",null),
     }).required(),
 });
